b41686abf60d67f09bc959dce99688e8319a69b1
AaronK1998 <l00169995@atu.ie>
2023-04-04 21:08:51 +0100

    Adding Services

--- BugTrackingProject/Data/BugService.cs
+++ BugTrackingProject/Data/BugService.cs
@@ -1,18 +1,95 @@
-﻿using Microsoft.AspNetCore.Server.IIS.Core;
+﻿using BugTrackingProject.Models;
+using Microsoft.AspNetCore.Server.IIS.Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugTrackingProject.Data
 {
     public class BugService : IBugService
     {
-        /*readonly ApplicationDbContext _dbContext = new();
-        public ClubManager(DatabaseContext dbContext)
+        private readonly ApplicationDbContext _dbContext = new();
+
+        public BugService(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
         }
-        public List<Bug> getBugs()
+
+        public void AddBug(Bug bug)
         {
-            throw new NotImplementedException();
-        }*/
-        
+            try
+            {
+                _dbContext.Bugs.Add(bug);
+                _dbContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void DeleteBug(int id)
+        {
+            try
+            {
+                Bug? bug = _dbContext.Bugs.Find(id);
+                if (bug != null)
+                {
+                    _dbContext.Bugs.Remove(bug);
+                    _dbContext.SaveChanges();
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public Bug GetBugData(int id)
+        {
+            try
+            {
+                Bug? bug = _dbContext.Bugs.Find(id);
+                if (bug != null)
+                {
+                    return bug;
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<Bug> GetBugDetails()
+        {
+            try
+            {
+                return _dbContext.Bugs.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void UpdateBugDetails(Bug bug)
+        {
+            try
+            {
+                _dbContext.Entry(bug).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
     }
 }
--- BugTrackingProject/Data/IBugService.cs
+++ BugTrackingProject/Data/IBugService.cs
@@ -1,7 +1,13 @@
-﻿namespace BugTrackingProject.Data
+﻿using BugTrackingProject.Models;
+
+namespace BugTrackingProject.Data
 {
     public interface IBugService
     {
-        
+        public List<Bug> GetBugDetails();
+        public void AddBug(Bug bug);
+        public void UpdateBugDetails(Bug bug);
+        public Bug GetBugData(int id);
+        public void DeleteBug(int id);
     }
 }
